075815b13c8050e066d0dce3fe5097dd0fec39b0
nelsonRoberts19 <22997628-nelsonRoberts19@users.noreply.replit.com>
2024-12-10 06:27:21 +0000

    Agent query: Is the home page now loading correctly with the sidebar and content, without any 404 errors?

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'wouter';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/lib/queryClient';
-import { Toaster } from '@/components/ui/toaster';
-
-// Import layouts and pages
 import MainLayout from '@/layouts/MainLayout';
 import HomePage from '@/pages/HomePage';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MainLayout>
-        <Switch>
-          <Route path="/" component={HomePage} />
-        </Switch>
-      </MainLayout>
-      <Toaster />
-    </QueryClientProvider>
+    <MainLayout>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route>404 - Not Found</Route>
+      </Switch>
+    </MainLayout>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Switch, Route } from "wouter";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
-
-function Router() {
-  return (
-    <Switch>
-      {/* <Route path="/" component={App} /> */}
-      <Route>404 Page Not Found</Route>
-    </Switch>
-  );
-}
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <App />
       <Toaster />
     </QueryClientProvider>
   </StrictMode>,
